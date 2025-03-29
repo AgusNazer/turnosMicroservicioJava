595c9fac6f2b5185e1d53ec4e3445b8e63ef817c
agusNazer <agustin.nazer@hotmail.com>
2025-03-28 21:22:14 -0300

    service turno

--- src/main/java/com/agusdev/turnos/service/ITurnoService.java
+++ src/main/java/com/agusdev/turnos/service/ITurnoService.java
@@ -1,5 +1,18 @@
 package com.agusdev.turnos.service;
 
-public class ITurnoService {
+import java.util.List;
 
+import com.agusdev.turnos.model.Turno;
+
+public interface ITurnoService {
+
+    public List<Turno> getTurnos();
+
+    public void saveTurno(Turno turno);
+
+    public void editTurno(Long id, Turno turno);
+
+    public Turno findTurno(Long id);
+
+    public Turno deleteTurno(Long id);
 }
--- src/main/java/com/agusdev/turnos/service/TurnoService.java
+++ src/main/java/com/agusdev/turnos/service/TurnoService.java
@@ -1,5 +1,50 @@
 package com.agusdev.turnos.service;
 
-public class TurnoService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.agusdev.turnos.model.Turno;
+import com.agusdev.turnos.repository.ITurnoRepository;
+
+@Service
+public class TurnoService implements ITurnoService {
+
+    @Autowired
+    private ITurnoRepository turnoRepository;
+
+    @Override
+    public List<Turno> getTurnos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTurnos'");
+    }
+
+    @Override
+    public void saveTurno(Turno turno) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationExceptions("Unimplemented method 'saveTurno'");
+    }
+
+    @Override
+    public void editTurno(Long id, Turno turno) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'editTurno'");
+    }
+
+    @Override
+    public Turno findTurno(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findTurno'");
+    }
+
+    @Override
+    public Turno deleteTurno(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteTurno'");
+    }
+
+    
+
+    
 }

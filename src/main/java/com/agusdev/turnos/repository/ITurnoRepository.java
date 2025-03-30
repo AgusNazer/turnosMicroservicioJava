1f5d1ab702459a5057edef28f8d74230c39f93f1
agusNazer <agustin.nazer@hotmail.com>
2025-03-29 21:10:42 -0300

    feature - repository jpa extended

--- src/main/java/com/agusdev/turnos/repository/ITurnoRepository.java
+++ src/main/java/com/agusdev/turnos/repository/ITurnoRepository.java
@@ -1,5 +1,21 @@
 package com.agusdev.turnos.repository;
 
-public interface ITurno {
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.agusdev.turnos.model.Turno;
+
+public interface ITurnoRepository extends JpaRepository<Turno, Long>{
+
+    public List<Turno> getTurnos();
+    
+    public void saveTUrno(Date fecha, String tratamiento, String dniPaciente);
+    
+    public void deleteTUrno(Long id);
+
+    public Turno findTurno(Long id);
+
+    public void editTurno(Long id, Turno turno);
 }

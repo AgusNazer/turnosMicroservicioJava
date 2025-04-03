860fa925265fc58bb8492d48813828a685a137b3
agusNazer <agustin.nazer@hotmail.com>
2025-04-02 23:32:50 -0300

    clean code

--- src/main/java/com/agusdev/turnos/repository/ITurnoRepository.java
+++ src/main/java/com/agusdev/turnos/repository/ITurnoRepository.java
@@ -7,13 +7,4 @@ import com.agusdev.turnos.model.Turno;
 
 public interface ITurnoRepository extends JpaRepository<Turno, Long>{
 
-    // public  List<Turno> getTurnos();
-    
-    // public void saveTurno(LocalDate fecha, String tratamiento, String dniPaciente);
-    
-    // public void deleteTurno(Long id);
-
-    // public Turno findTurno(Long id);
-
-    // public void editTurno(Long id, Turno turno);
 }
